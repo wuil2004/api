16843ff1c0b579c8e5df8485a44725ba9e5bdf4a
wuil2004 <williamsmatiassan@gmail.com>
2025-01-15 12:23:40 -0600

    ok

--- app.py
+++ app.py
@@ -126,4 +126,6 @@ def servir_imagen(nombre):
 
 # Iniciar la aplicación
 if __name__ == '__main__':
-    app.run(debug=True)
+    host = '0.0.0.0'
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host=host, port=port, debug=True)
